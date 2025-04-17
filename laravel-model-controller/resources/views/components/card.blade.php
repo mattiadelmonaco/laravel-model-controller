85d7667063e490f8d46b34dedab8210d302e64bd
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-17 17:15:08 +0200

    feat: add component card, get every movie in home

---
+++ laravel-model-controller/resources/views/components/card.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h3><strong>Titolo: </strong>{{ $title }}</h3>
+    <h4><strong>Titolo originale: </strong>({{$original_title}})</h4>
+    <h5><strong>Data di uscita: </strong>{{ $date }}</h5>
+    <h5><strong>Paese di produzione: </strong>{{ $nationality }}</h5>
+    <h6><strong>Media voto degli uteti: </strong>{{$rating}}</h6>
+</div>
--- laravel-model-controller/resources/views/home.blade.php
+++ laravel-model-controller/resources/views/home.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.master')
 
 @section("content")
-<div>ciao</div>
+
+<ul class="list-group">
+@foreach ($movies as $movie)
+    
+    <li class="list-group-item">
+        <x-card>
+            <x-slot:title>
+                {{$movie["title"]}}
+            </x-slot:title>
+            <x-slot:original_title>
+                {{$movie["original_title"]}}
+            </x-slot:original_title>
+            <x-slot:date>
+                {{$movie["date"]}}
+            </x-slot:date>
+            <x-slot:nationality>
+                {{$movie["nationality"]}}
+            </x-slot:nationality>
+            <x-slot:rating>
+                {{$movie["vote"]}}
+            </x-slot:rating>
+
+
+        </x-card>
+    </li>
+
+@endforeach
+</ul>
+
 @endsection
